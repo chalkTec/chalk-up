eb0b410999c0a0686ee39e998c34794527169e32
Florian Rampp <Florian.Rampp@gmail.com>
2014-03-19 15:02:15 +0100

    angular generator output

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('chalkUpApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
