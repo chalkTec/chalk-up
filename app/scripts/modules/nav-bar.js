414c4cd0a9ea3a496eaa8cbfbd1d13290a354636
Florian Rampp <Florian.Rampp@gmail.com>
2014-04-08 15:54:54 +0200

    nav-bar directive now transcludes buttons

--- app/scripts/modules/nav-bar.js
+++ app/scripts/modules/nav-bar.js
@@ -2,57 +2,13 @@
 
 angular.module('navBar', []);
 
-
-angular.module('navBar')
-	.factory('navBarService', function LoadingIndicator($rootScope) {
-		function clearNavBar() {
-			$rootScope.navBar = {
-				buttons: []
-			};
-		}
-
-		// initialize data structure
-		clearNavBar();
-
-		$rootScope.$on('$stateChangeStart', function () {
-			clearNavBar();
-		});
-
-		var config = {
-			addButton: function (button) {
-				$rootScope.navBar.buttons.push(button);
-			},
-			clearNavBar: clearNavBar
-		};
-
-		return config;
-	});
-
-
 angular.module('navBar')
 	.directive('navBar', function () {
 
 		return {
 			restrict: 'E',
 			replace: true,
-			templateUrl: '/views/modules/nav-bar.html',
-			scope: true,
-			controller: function ($scope, $state) {
-				function execute(item) {
-					if (_.has(item, 'action')) {
-						item.action();
-					}
-					else if (_.has(item, 'state')) {
-						$state.go(item.state);
-					}
-					else {
-						throw new Error('button must either define an action or a state');
-					}
-				}
-
-				$scope.buttonClick = function (button) {
-					execute(button);
-				};
-			}
+			transclude: true,
+			templateUrl: '/views/modules/nav-bar.html'
 		};
 	});
