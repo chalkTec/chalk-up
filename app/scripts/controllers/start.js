85d202845b09b03c417adce22529cc5b5656c1f0
Florian Rampp <Florian.Rampp@gmail.com>
2014-04-23 10:23:52 +0200

    auto login when editing demo gym

--- app/scripts/controllers/start.js
+++ app/scripts/controllers/start.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('chalkUpApp')
-	.controller('StartCtrl', function ($scope, Restangular) {
+	.controller('StartCtrl', function ($scope, $state, Restangular, user) {
 		$scope.gyms = Restangular.all('gyms').getList().$object;
+
+		$scope.demoGymClick = function (gym) {
+			if (user.current) {
+				user.logout();
+			}
+			user.login({login: 'demo@chalkup.de', password: 'abcdef'}, function (error) {
+				if(error) {
+					console.log(error);
+				}
+				else {
+					$state.transitionTo('admin', {id: gym.id});
+				}
+			});
+		};
 	});
