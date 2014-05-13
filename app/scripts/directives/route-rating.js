d5bf7034bb8cc5be7d7d14c17816a3b1b4bf8b63
Florian Rampp <Florian.Rampp@gmail.com>
2014-05-13 18:45:16 +0200

    changed title of and brackets behind stars
    brackets contain number of votes now
    mouseover contains average value

--- app/scripts/directives/route-rating.js
+++ app/scripts/directives/route-rating.js
@@ -5,13 +5,13 @@ angular.module('chalkUpApp')
 
 		return {
 			restrict: 'A',
-			template: '<rating class="route-rating" value="value" readonly="readonly" on-hover="overStar = value" on-leave="overStar = null" ng-click="rate(overStar)" title="{{routeRating.count}} Stimmen" state-on="\'fi-star filled\'" state-off="\'fi-star unfilled\'" ng-class="{\'not-rated\': routeRating.count == 0}"></rating>',
+			template: '<rating class="route-rating" value="value" readonly="readonly" on-hover="overStar = value" on-leave="overStar = null" ng-click="rate(overStar)" title="{{mouseOverText}}" state-on="\'fi-star filled\'" state-off="\'fi-star unfilled\'" ng-class="{\'not-rated\': routeRating.count == 0}"></rating>',
 			scope: {
 				routeRating: '=',
 				readonly: '=',
 				onRate: '&'
 			},
-			controller: function ($scope) {
+			controller: function ($scope, $filter) {
 				$scope.$watch('routeRating', function(routeRating) {
 					if(!_.isUndefined(routeRating)) {
 						$scope.value = routeRating.average;
@@ -27,9 +27,12 @@ angular.module('chalkUpApp')
 						$scope.rate = function(rating) {
 							$scope.onRate({rating: rating});
 						};
+						$scope.mouseOverText = 'stimme ab!';
 					}
 					else {
 						$scope.rate = function() {};
+						var avg = $scope.routeRating.average;
+						$scope.mouseOverText = avg ? $filter('number')(avg, 1) + ' Sterne' : 'keine Stimmen';
 					}
 				});
 			}
